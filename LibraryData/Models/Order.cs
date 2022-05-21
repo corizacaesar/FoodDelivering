d27ba50b1de1b34c391c66ecc7f9fe91d29a9841
Coriza Caesarchakti <47585721+corizacaesar@users.noreply.github.com>
2022-05-21 16:27:04 +0700

    Add Docker Compose

---
+++ LibraryData/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Models
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public int UserId { get; set; }
+        public string? Latitude { get; set; }
+        public string? Longitude { get; set; }
+        public int? CourierId { get; set; }
+        public bool Status { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual User User { get; set; } = null!;
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
